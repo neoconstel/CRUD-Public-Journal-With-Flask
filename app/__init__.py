1386a5bb4ec09fee274d7678be482c2c6b5eaf90
neoconstel <neoconstel@gmail.com>
2022-08-19 19:11:27 +0100

    modified flask shell context

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
-
 from flask import Flask, redirect, url_for
 from .models import db, User, Journal
+from .config import config
 
 # import blueprints
 from .views.home.routes import home_bp
 
 
-def create_app(config):
+def create_app(app_config):
     app = Flask(__name__)
-    app.config.from_object(config)
+    app.config.from_object(app_config)
 
 
     @app.route("/")
@@ -19,7 +19,8 @@ def create_app(config):
     # set automatic imports while running app with flask shell
     @app.shell_context_processor
     def load_in_shell():
-        return dict(db=db, User=User, Journal=Journal)
+        return dict(db=db, User=User, Journal=Journal,
+                                        config=config, create_app=create_app)
 
 
     # register blueprints
