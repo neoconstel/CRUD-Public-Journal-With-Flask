68b90e95b7796407d0734c0da98b2977e7ccaae1
neoconstel <neoconstel@gmail.com>
2022-08-19 18:02:59 +0100

    set auto-imports for flask shell

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, redirect, url_for
+from .models import db, User, Journal
 
 # import blueprints
 from .views.home.routes import home_bp
@@ -14,12 +15,19 @@ def create_app(config):
     def rootpath():
         return redirect(url_for("home.homepage"))
 
+    
+    # set automatic imports while running app with flask shell
+    @app.shell_context_processor
+    def load_in_shell():
+        return dict(db=db, User=User, Journal=Journal)
+
 
     # register blueprints
     app.register_blueprint(home_bp)
 
 
+    # initialize extensions
+    db.init_app(app)
+
+
     return app
-
-
-    
