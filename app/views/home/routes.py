34486b876ec46f83642eaece71ce95cb945fb067
neoconstel <neoconstel@gmail.com>
2022-08-19 20:31:39 +0100

    add home template

--- app/views/home/routes.py
+++ app/views/home/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
 
-home_bp = Blueprint("home", __name__, url_prefix="/home")
+home_bp = Blueprint("home", __name__, url_prefix="/home",
+                        template_folder="templates", static_folder="static")
 
 
 @home_bp.route("/")
 def homepage():
-    return "<h1>This is the homepage</h1>"
+    return render_template("index.html")
+    
