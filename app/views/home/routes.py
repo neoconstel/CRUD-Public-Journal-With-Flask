f623e1034f70a853f025c3991c787d3aaf9dc385
neoconstel <neoconstel@gmail.com>
2022-08-20 23:58:22 +0100

    show only current user's and public journals

--- app/views/home/routes.py
+++ app/views/home/routes.py
@@ -1,13 +1,15 @@
+from operator import xor
 from flask import Blueprint, render_template
 from app.models import User, Journal
+from flask_login import current_user
 
 home_bp = Blueprint("home", __name__, url_prefix="/home",
                         template_folder="templates", static_folder="static")
 
 
 @home_bp.route("/")
-def homepage():
-    journals = Journal.query.all()
-
+def homepage():    
+    user = User.query.filter(User.username==current_user.username).first()
+    journals = Journal.query.filter( (Journal.author==user) | (Journal.is_private==None) | (Journal.is_private==False) ).all()
     return render_template("index.html", journals=journals)
     
